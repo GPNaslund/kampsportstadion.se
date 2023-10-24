5181cbdfe4ebaee348696c8583498f54b79a7221
Gustav <gpnaslund@gmail.com>
2023-10-24 15:33:30 +0200

    Adds another class

--- src/data/classes.tsx
+++ src/data/classes.tsx
@@ -1,36 +1,60 @@
 const classes = [
-  {header: `Muay Thai`, text:`Här lär du dig grunderna i thaiboxning. Slag, spark, armbågar,
+  {
+    header: `Muay Thai`, text: `Här lär du dig grunderna i thaiboxning. Slag, spark, armbågar,
   knän och clinch. En utmanande och rolig sport där man ständigt utvecklas och lär sig något nytt. Denna träningsform passar båda motionärer och proffs.
-  En bra all around  träning med mycket fokus på fys, funktionell styrka och teknik.`, classLevels:`Barn (7-12 år), ungdomar (12-16år), nybörjare, fortsättning & avancerad.`, coaches:``,
+  En bra all around  träning med mycket fokus på fys, funktionell styrka och teknik.`, classLevels: `Barn (7-12 år), ungdomar (12-16år), nybörjare, fortsättning & avancerad.`, coaches: ``,
   },
-  {header: `MMA`, text:`Mixed Martial Arts (MMA) är en sport med sparkar, slag, kast och markkamp med inriktning på fullkontakt. Det är en teknisk sport var man ständigt utmanas, utvecklas och lär sig något nytt.
-  Handskar, benskydd och tandskydd behövs.`, classLevels:`alla nivåer`, coaches:``
+  {
+    header: `MMA`, text: `Mixed Martial Arts (MMA) är en sport med sparkar, slag, kast och markkamp med inriktning på fullkontakt. Det är en teknisk sport var man ständigt utmanas, utvecklas och lär sig något nytt.
+  Handskar, benskydd och tandskydd behövs.`, classLevels: `alla nivåer`, coaches: ``
   },
-  {header: `Brasiliansk jiu-jitsu (BJJ)`, text:`Brasiliansk jiu-jitsu (Bjj) härstammar från japansk jiu-jitsu och judo men har under decennier vidareutvecklats av familjen Gracie. Bjj är en grappling-stil, 
+  {
+    header: `Brasiliansk jiu-jitsu (BJJ)`, text: `Brasiliansk jiu-jitsu (Bjj) härstammar från japansk jiu-jitsu och judo men har under decennier vidareutvecklats av familjen Gracie. Bjj är en grappling-stil, 
   vilket innebär att man inte använder sig av slag eller sparkar utan istället fokuserar på att med ledlås och strypningar oskadliggöra sin motståndare. Grundtanken är att få ner sin motståndare på marken, 
   kontrollera honom/henne där och med hjälp av ledlås och strypningar tvinga denne att ge upp. Bjj är den kampsport där en mindre utövare har störst möjlighet att övermanna en större och starkare motståndare. 
-  Detta för att teknik alltid går före fysiska egenskapar. Fokus på teknik och att kampen huvudsaklig pågår på marken gör att detta är en sport som passar alla. Killar som tjejer, små som stora och unga som gamla.`, 
-  classLevels:`Barn (7-12 år), ungdomar (12-16år), nybörjare, fortsättning & avancerad.`, coaches:``},
-  {header: `Kampsportsfys`, text:`Funktionell styrka med kampsportsmoment. Effektiv och rolig träning. Utmanande utan att man behöver slåss med någon. Passar för alla och inga förkunskaper krävs.`, 
-  classLevels:``, coaches:``},
-  {header: `Dunkardax/Cirkelfys`, text:`90 minuter cirkelträning med kampsport moment. Ett fyspass med högt tempo, styrka och flås! Passar för alla och inga förkunskaper krävs.`, 
-  classLevels:``, coaches:``},
-  {header: `Gym`, text:`I vårt gym hittar du allt du behöver för att bygga upp styrka och stabilitet. Vi har löpband, roddmaskiner och cyklar samt skivstänger, hantlar, kettlebells och kabelmaskin för att du ska kunna kunna få en varierad träning som komplement till kampsporten.`, 
-  classLevels:``, coaches:``},
-  {header: `Kampsport Barngrupp`, text:`Rekommenderad ålder är ca 7 till 11 år. BJJ: Brottning med GI-dräkt, Måndagar 17.00-17.55. Thaiboxning: Blandning av tekniker som slag och sparkar med fys och kroppskontroll, Onsdagar 17.00-17.45 + Lördagar 12.00-13.00. Kampsportlek: Kampsport för dom minsta. Barnen kommer lära sig brottning slag och sparkar under kontrollerade lekfulla former, 4-6 år: Lördagar 12.00-12.45, 
-  Föräldrarna til dom minsta behöver vara med och hjälpa till att assistera under passen. Inga pass under skolloven.`, 
-  classLevels:``, coaches:``},
-  {header: `AW/After work`, text:`Byt ut ölen med en Nocco och dra med kollegor och vänner på ett After Workout pass! 60 minuters grym träning!
-  Vill ditt företag köra en After Workout:maila till info@kampsportstadion.se`, 
-  classLevels:``, coaches:``},
-  {header: `Small Group`, text:`Vill ni köra i en ännu mindre grupp och mer personligt anpassad träning kan vi även erbjuda detta. Ni kör med en PT i en grupp upp till 4pers, där ni har chans att påverka varje pass utefter era önskemål.
-  Det kostar 1800kr per tillfälle och intresseanmälan görs till info@kampsportstadion.se`, 
-  classLevels:``, coaches:``},
-  {header: `Strong Mama`, text:`Här är du som har en bebis hemma från 0-6mån välkommen att delta tillsammans med ditt lilla mirakel och andra mammor.
+  Detta för att teknik alltid går före fysiska egenskapar. Fokus på teknik och att kampen huvudsaklig pågår på marken gör att detta är en sport som passar alla. Killar som tjejer, små som stora och unga som gamla.`,
+    classLevels: `Barn (7-12 år), ungdomar (12-16år), nybörjare, fortsättning & avancerad.`, coaches: ``
+  },
+  {
+    header: `Kampsportsfys`, text: `Funktionell styrka med kampsportsmoment. Effektiv och rolig träning. Utmanande utan att man behöver slåss med någon. Passar för alla och inga förkunskaper krävs.`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `Dunkardax/Cirkelfys`, text: `90 minuter cirkelträning med kampsport moment. Ett fyspass med högt tempo, styrka och flås! Passar för alla och inga förkunskaper krävs.`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `Gym`, text: `I vårt gym hittar du allt du behöver för att bygga upp styrka och stabilitet. Vi har löpband, roddmaskiner och cyklar samt skivstänger, hantlar, kettlebells och kabelmaskin för att du ska kunna kunna få en varierad träning som komplement till kampsporten.`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `Kampsport Barngrupp`, text: `Rekommenderad ålder är ca 7 till 11 år. BJJ: Brottning med GI-dräkt, Måndagar 17.00-17.55. Thaiboxning: Blandning av tekniker som slag och sparkar med fys och kroppskontroll, Onsdagar 17.00-17.45 + Lördagar 12.00-13.00. Kampsportlek: Kampsport för dom minsta. Barnen kommer lära sig brottning slag och sparkar under kontrollerade lekfulla former, 4-6 år: Lördagar 12.00-12.45, 
+  Föräldrarna til dom minsta behöver vara med och hjälpa till att assistera under passen. Inga pass under skolloven.`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `AW/After work`, text: `Byt ut ölen med en Nocco och dra med kollegor och vänner på ett After Workout pass! 60 minuters grym träning!
+  Vill ditt företag köra en After Workout:maila till info@kampsportstadion.se`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `Small Group`, text: `Vill ni köra i en ännu mindre grupp och mer personligt anpassad träning kan vi även erbjuda detta. Ni kör med en PT i en grupp upp till 4pers, där ni har chans att påverka varje pass utefter era önskemål.
+  Det kostar 1800kr per tillfälle och intresseanmälan görs till info@kampsportstadion.se`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `Strong Mama`, text: `Här är du som har en bebis hemma från 0-6mån välkommen att delta tillsammans med ditt lilla mirakel och andra mammor.
   Vi kommer jobba utifrån ditt utgångsläge, men tillsammans med andra mammor för att skapa en liten exklusiv grupp för nyblivna mammor med samma intresse.
   Fokus ligger här på att få tillbaka core-stabilitet och succesivt jobba upp din styrka och kondition.
-  Vi kommer jobba tillsammans 2ggr/vecka i 4-8veckor och detta pass ligger utanför ordinare medlemskap - För mer info och pris maila info@kampsportstadion.se och skriv StrongMama i ämnesfältet.`, 
-  classLevels:``, coaches:``}
+  Vi kommer jobba tillsammans 2ggr/vecka i 4-8veckor och detta pass ligger utanför ordinare medlemskap - För mer info och pris maila info@kampsportstadion.se och skriv StrongMama i ämnesfältet.`,
+    classLevels: ``, coaches: ``
+  },
+  {
+    header: `Barnkalas`, text: `Vill ni ha ett barnkalas mer riktat mot hälsa, rörelseglädje och bus så har ni nu möjlighet att boka vår sal.
+Längd på kalaset och vad ni vill att det innehåller kommer vi gemensamt fram till, och priset läggs upp utefter vad ni har för önskemål (Längd på kalaset, om instruktör ska ingå, mat/dryck till gästerna, eventuella övriga önskemål)
+Vi rekommenderar 90 min där ca 45min är aktivitet och 45 min fika/fri lek.
+Grundpris utan instruktör 3500, med instruktör 4500. Maila info@kampsportstadion.se för mer info och bokning`, classLevels: ``, coaches: ``
+  }
 ]
 
 export default classes;
